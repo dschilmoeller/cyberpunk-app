8e2acd6740eca5de492721548da21605e1d5f190
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-06-26 19:27:43 -0500

    added more responsive luck setup.

--- src/redux/reducers/characterStatus.reducer.js
+++ src/redux/reducers/characterStatus.reducer.js
@@ -3,7 +3,7 @@ const characterStatus = (state={
     current_lethal: 0,
     current_agg: 0,
     current_armor_loss: 0,
-    current_luck: 0
+    current_luck_loss: 0
 }, action) => {
     if (action.type === "SET_CHARACTER_STATUS") {
         return action.payload;
@@ -20,12 +20,21 @@ const characterStatus = (state={
     if (action.type === "REMOVE_WOUND") {
         return {...state, current_agg: state.current_agg-1}
     }
+    
     if (action.type === "REMOVE_ONE_ARMOR") {
         return {...state, current_armor_loss: state.current_armor_loss +1}
     }
     if (action.type === "ADD_ONE_ARMOR") {
         return {...state, current_armor_loss: state.current_armor_loss -1}
     }
+    
+    if (action.type === "REMOVE_ONE_LUCK") {
+        return {...state, current_luck_loss: state.current_luck_loss +1}
+    }
+    if (action.type === "ADD_ONE_LUCK") {
+        return {...state, current_luck_loss: state.current_luck_loss -1}
+    }
+    
 
     if (action.type === "CLEAR_CHARACTER_STATUS") {
         return {
@@ -33,7 +42,7 @@ const characterStatus = (state={
     current_lethal: 0,
     current_agg: 0,
     current_armor_loss: 0,
-    current_luck: 0
+    current_luck_loss: 0
 };
     }
     return state
