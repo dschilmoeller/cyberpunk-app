023126c6b61ec854c383e8ed847a49e1e0fc3b73
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-07-04 12:07:51 -0500

    combat, technical, and role abilities added to character sheet. Role ability appearances made conditional to accommodate spacing needs.

--- src/components/Characters/CharacterSheet/CharacterSpecialSkills.jsx
+++ src/components/Characters/CharacterSheet/CharacterSpecialSkills.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
-import { Stack } from '@mui/material';
 import Item from './Item';
 
 function CharacterSpecialSkills(charDetailProp) {
@@ -72,21 +71,21 @@ function CharacterSpecialSkills(charDetailProp) {
         setAirVehicleTech(dotReturn(charDetail.air_tech))
         setSeaVehicleTech(dotReturn(charDetail.sea_tech))
 
-        setRockerboy(dotReturn(charDetail.rockerboy))
-        setSolo(dotReturn(charDetail.solo))
-        setNetrunner(dotReturn(charDetail.netrunner))
-        setNomad(dotReturn(charDetail.nomad))
-        setMedia(dotReturn(charDetail.media))
-        setMedtech(dotReturn(charDetail.medtech))
-        setMaker(dotReturn(charDetail.maker))
+        setRockerboy(roleDotReturn(charDetail.rockerboy))
+        setSolo(roleDotReturn(charDetail.solo))
+        setNetrunner(roleDotReturn(charDetail.netrunner))
+        setNomad(roleDotReturn(charDetail.nomad))
+        setMedia(roleDotReturn(charDetail.media))
+        setMedtech(roleDotReturn(charDetail.medtech))
+        setMaker(roleDotReturn(charDetail.maker))
 
         setMedSurgery(dotReturn(charDetail.med_surgery))
         setMedPharma(dotReturn(charDetail.med_pharma))
         setMedCryo(dotReturn(charDetail.med_cryo))
-        setMakerField(dotReturn(charDetail.maker_field))
-        setMakerUpgrade(dotReturn(charDetail.maker_upgrade))
-        setMakerFab(dotReturn(charDetail.maker_fab))
-        setMakerInvent(dotReturn(charDetail.maker_invent))
+        setMakerField(makerDotReturn(charDetail.maker_field))
+        setMakerUpgrade(makerDotReturn(charDetail.maker_upgrade))
+        setMakerFab(makerDotReturn(charDetail.maker_fab))
+        setMakerInvent(makerDotReturn(charDetail.maker_invent))
     }
 
     const dotReturn = (skill) => {
@@ -100,7 +99,33 @@ function CharacterSpecialSkills(charDetailProp) {
             returnedDots += emptydot
         }
         return returnedDots
+    }
 
+    const roleDotReturn = (skill) => {
+        let returnedDots = ''
+        if (skill != 0) {
+            for (let i = 0; i < skill; i++) {
+                returnedDots += fulldot;
+            }
+            let j = skill
+            for (j; j <= 9; j++) {
+                returnedDots += emptydot
+            }
+        }
+        return returnedDots
+    }
+
+    const makerDotReturn = (skill) => {
+        let returnedDots = ''
+
+        for (let i = 0; i < skill; i++) {
+            returnedDots += fulldot;
+        }
+        let j = skill
+        for (j; j <= 9; j++) {
+            returnedDots += emptydot
+        }
+        return returnedDots
     }
 
     return (
@@ -111,6 +136,7 @@ function CharacterSpecialSkills(charDetailProp) {
                     <Grid item xs={12}>
                         <Item>Combat Skills</Item>
                     </Grid>
+
                     <Grid item xs={4.5}>
                         <Item>Evasion</Item>
                     </Grid>
@@ -159,6 +185,7 @@ function CharacterSpecialSkills(charDetailProp) {
                     <Grid item xs={7.5}>
                         <Item>{heavyWeapons}</Item>
                     </Grid>
+
                 </Grid>
             </Grid>
 
@@ -228,54 +255,113 @@ function CharacterSpecialSkills(charDetailProp) {
                 </Grid>
             </Grid>
 
-
             <Grid item xs={4}>
                 <Grid container spacing={1}>
                     <Grid item xs={12}>
                         <Item>Role Abilities</Item>
                     </Grid>
-                    <Grid item xs={4.5}>
-                        <Item>Rockerboy</Item>
-                    </Grid>
-                    <Grid item xs={7.5}>
-                        <Item>{rockerboy}</Item>
-                    </Grid>
-                    <Grid item xs={4.5}>
-                        <Item>Solo</Item>
-                    </Grid>
-                    <Grid item xs={7.5}>
-                        <Item>{solo}</Item>
-                    </Grid>
-                    <Grid item xs={4.5}>
-                        <Item>Netrunner</Item>
-                    </Grid>
-                    <Grid item xs={7.5}>
-                        <Item>{netrunner}</Item>
-                    </Grid>
-                    <Grid item xs={4.5}>
-                        <Item>Nomad</Item>
-                    </Grid>
-                    <Grid item xs={7.5}>
-                        <Item>{nomad}</Item>
-                    </Grid>
-                    <Grid item xs={4.5}>
-                        <Item>Media</Item>
-                    </Grid>
-                    <Grid item xs={7.5}>
-                        <Item>{media}</Item>
-                    </Grid>
-                    <Grid item xs={4.5}>
-                        <Item>Medtech</Item>
-                    </Grid>
-                    <Grid item xs={7.5}>
-                        <Item>{medtech}</Item>
-                    </Grid>
-                    <Grid item xs={4.5}>
+                    {rockerboy != '' ? (<>
+                        <Grid item xs={4.5}>
+                            <Item>Rockerboy</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{rockerboy}</Item>
+                        </Grid></>) : <></>}
+
+                    {solo != '' ? (<>
+                        <Grid item xs={4.5}>
+                            <Item>Solo</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{solo}</Item>
+                        </Grid></>) : <></>}
+
+                    {netrunner != '' ? (<>
+                        <Grid item xs={4.5}>
+                            <Item>Netrunner</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{netrunner}</Item>
+                        </Grid></>) : <></>}
+
+                    {nomad != '' ? (<>
+                        <Grid item xs={4.5}>
+                            <Item>Nomad</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{nomad}</Item>
+                        </Grid></>) : <></>}
+
+                    {media != '' ? (<>
+                        <Grid item xs={4.5}>
+                            <Item>Media</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{media}</Item>
+                        </Grid></>) : <></>}
+
+                    {medtech != '' ? (<>
+                        <Grid item xs={4.5}>
+                            <Item>Medtech</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{medtech}</Item>
+                        </Grid>
+
+                        <Grid item xs={4.5}>
+                            <Item>Surgery</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{medSurgery}</Item>
+                        </Grid>
+
+                        <Grid item xs={4.5}>
+                            <Item>Pharmaceuticals</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{medPharma}</Item>
+                        </Grid>
+
+                        <Grid item xs={4.5}>
+                            <Item>Cryo</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{medCryo}</Item>
+                        </Grid>
+                    </>) : <></>}
+
+                    {maker != '' ? (<><Grid item xs={4.5}>
                         <Item>Maker</Item>
                     </Grid>
-                    <Grid item xs={7.5}>
-                        <Item>{maker}</Item>
-                    </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{maker}</Item>
+                        </Grid>
+                        <Grid item xs={4.5}>
+                            <Item>Field</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{makerField}</Item>
+                        </Grid>
+                        <Grid item xs={4.5}>
+                            <Item>Upgrade</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{makerUpgrade}</Item>
+                        </Grid>
+                        <Grid item xs={4.5}>
+                            <Item>Fab</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{makerFab}</Item>
+                        </Grid>
+                        <Grid item xs={4.5}>
+                            <Item>Invent</Item>
+                        </Grid>
+                        <Grid item xs={7.5}>
+                            <Item>{makerInvent}</Item>
+                        </Grid>
+                        </>) : <></>}
+
                     <Grid item xs={12} />
 
                 </Grid>
