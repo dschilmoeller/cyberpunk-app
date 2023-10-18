0a2e427f77bf76306c28d33bd570de940744c7ac
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-10-18 10:27:06 -0500

    minor about page change.

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -3,7 +3,8 @@ import { Grid, Link } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
-function AboutPage() {
+
+export default function AboutPage() {
   return (
     <>
       <Grid container spacing={1} >
@@ -40,5 +41,3 @@ function AboutPage() {
     </>
   );
 }
-
-export default AboutPage;
