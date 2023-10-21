926a303dc72d61065358a469ace6233618a4f98f
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-10-20 19:17:02 -0500

    kaboom

--- src/components/Characters/CharacterSheet/WeaponsGrenades.jsx
+++ src/components/Characters/CharacterSheet/WeaponsGrenades.jsx
@@ -3,24 +3,48 @@ import Grid from '@mui/material/Grid';
 import { useSelector, useDispatch } from 'react-redux';
 import Item from './Item';
 import { Button, Typography } from '@mui/material';
-import CircleOutlinedIcon from '@mui/icons-material/CircleOutlined';
-import CancelIcon from '@mui/icons-material/Cancel';
 
-import OtherAttributesDialog from '../../Modals/OtherAttributesDialog';
 import WeaponDialog from '../../Modals/WeaponDialog';
+
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import Slide from '@mui/material/Slide';
+
+
+function TransitionUp(props) {
+    return <Slide {...props} direction="up" />;
+}
 
 export default function WeaponsGrenades() {
     const charDetail = useSelector((store) => store.characterDetail)
     const charGrenades = useSelector((store) => store.characterGrenades)
     const dispatch = useDispatch();
 
+    const [showSnackbar, setShowSnackbar] = React.useState(false);
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
+
     const useGrenade = (grenade) => {
-        dispatch({type: 'USE_GRENADE', payload: grenade})
+        dispatch({ type: 'USE_GRENADE', payload: grenade })
+        setShowSnackbar(true)
     }
 
     const grenadeRange = (charDetail.strength + charDetail.cyber_strength) * 5
 
     return (<>
+        <Snackbar
+            TransitionComponent={TransitionUp}
+            autoHideDuration={2000}
+            open={showSnackbar}
+            onClose={() => setShowSnackbar(false)}
+            anchorOrigin={{ horizontal: 'center', vertical: 'top' }}
+        >
+            <Alert onClose={() => setShowSnackbar(false)} severity="error" sx={{ width: '100%' }}>
+                Kaboom!
+            </Alert>
+        </Snackbar >
+
         <Grid container>
             <Grid item xs={12}><Item>Grenades</Item></Grid>
             {charGrenades.map(grenade => {
