75cf9947186753bf439ade9d4fb9c1d065543187
David Schilmoeller <dave.schilmoeller@gmail.com>
2024-09-18 19:15:40 -0500

    removed small amount of wet code.

---
+++ src/utils/objects/objects.utils.js
@@ -0,0 +1,44 @@
+const SkillSet = {
+    streets: [
+        ['athletics', 'Athletics'],
+        ['brawling', 'Brawling'],
+        ['concentration', 'Concentration'],
+        ['evasion', 'Evasion'],
+        ['fast_talk', 'Fast Talk'],
+        ['firearms', 'Firearms'],
+        ['legerdemain', 'Legerdemain'],
+        ['melee_weapons', 'Melee Weapons'],
+        ['perception', 'Perception'],
+        ['streetwise', 'Streetwise'],
+    ],
+    tekhne: [
+        ['demolitions', 'Demolitions'],
+        ['drive_land', 'Drive Land'],
+        ['drive_exotic', 'Drive Exotic'],
+        ['etiquette', 'Etiquette'],
+        ['exotic_weapons', 'Exotic Weapons'],
+        ['heavy_weapons', 'Heavy Weapons'],
+        ['performance', 'Performance'],
+        ['stealth', 'Stealth'],
+        ['survival', 'Survival'],
+        ['tracking', 'Tracking'],
+    ],
+    knowledge: [
+        ['business', 'Business'],
+        ['cryptography', 'Cryptography'],
+        ['cyber_tech', 'Cyber Tech'],
+        ['investigation', 'Investigation'],
+        ['gambling', 'Gambling'],
+        ['language', 'Language'],
+        ['military_tech', 'Military Tech'],
+        ['science', 'Science'],
+        ['vehicle_tech', 'Vehicle Tech']
+    ],
+    paramed: [
+        ['is_paramed'],
+        ['first_aid', 'First Aid'],
+        ['paramedic', 'Paramedic'],
+    ]
+}
+
+export default SkillSet
